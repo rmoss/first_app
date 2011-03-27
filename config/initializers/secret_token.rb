ea6381c391173caa225cb562fa5f1ceb4609b200
RMM <rmm@HackBookRM-8.local>
2011-03-27 14:14:56 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+FirstApp::Application.config.secret_token = '9160df5281291f811cdd47b51080b59ddd56f2a22ba74eed7a2b29cb23a0be3a0a2673fd6d1881de4cede7584a8c2c26324de3278c5d2f57c7107682334297db'
